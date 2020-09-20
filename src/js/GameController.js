78337078844a6f66a1ada2037b2b76f21ef272c2
Vadim2107 <58651870+Vadim2107@users.noreply.github.com>
2020-09-20 19:21:24 +0300

    initial commit

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,12 +1,31 @@
+// import GamePlay from './GamePlay';
+import themes from './themes';
+import PositionedCharacter from './PositionedCharacter';
+import { characterGenerator, generatePosition, generateTeam, typeList } from './generators';
+import { getPropagation } from './utils';
+
 export default class GameController {
-  constructor(gamePlay, stateService) {
+  constructor(gamePlay, stateService, side) {
     this.gamePlay = gamePlay;
     this.stateService = stateService;
+    this.index = 0;
+    this.side = side;
   }
 
   init() {
-    // TODO: add event listeners to gamePlay events
-    // TODO: load saved stated from stateService
+    // GamePlay.drawUi('prairie');
+    this.gamePlay.drawUi('prairie');
+    this.typeList = (this.side === 'good') ? typeList : typeList.reverse();
+    this.teamP = generateTeam(typeList.slice(0, typeList.length / 2), 4, 5);
+    this.teamE = generateTeam(typeList.slice(typeList.length / 2), 4, 5);
+    this.position = [
+      ...generatePosition(this.teamP, this.gamePlay.boardSize, 'good'),
+      ...generatePosition(this.teamE, this.gamePlay.boardSize, 'evil')
+    ];
+    this.gamePlay.redrawPositions(this.position);
+    this.gamePlay.addCellEnterListener(this.onCellEnter.bind(this));
+    this.gamePlay.addCellLeaveListener(this.onCellLeave.bind(this));
+    this.gamePlay.addCellClickListener(this.onCellClick.bind(this));
   }
 
   onCellClick(index) {
